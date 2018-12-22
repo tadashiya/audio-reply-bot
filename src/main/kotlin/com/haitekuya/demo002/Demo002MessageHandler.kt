b73fd148259d4e3b1ed348adee8b86c18e9032db
Tadashi Yamasaki <yamasaki@haitekuya.com>
2018-12-23 01:50:29 +0900

    Use local server to host audio file

--- src/main/kotlin/com/haitekuya/demo002/Demo002MessageHandler.kt
+++ src/main/kotlin/com/haitekuya/demo002/Demo002MessageHandler.kt
@@ -4,13 +4,25 @@ import com.linecorp.bot.client.LineMessagingClient
 import com.linecorp.bot.model.ReplyMessage
 import com.linecorp.bot.model.event.MessageEvent
 import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.AudioMessage
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
+
 
 @LineMessageHandler
 class Demo002MessageHandler(val lineMessagingClient: LineMessagingClient) {
+
+    private val url = "https://test.abc/audio/"
 
     @EventMapping
     fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>) {
@@ -27,17 +39,32 @@ class Demo002MessageHandler(val lineMessagingClient: LineMessagingClient) {
 
         // Download Movie
         val tmpFileName = UUID.randomUUID().toString() + ".m4a"
-        val command = "youtube-dl -x --audio-format m4a -o $tmpFileName $text"
+        val command = "youtube-dl -x --audio-format m4a -o /tmp/$tmpFileName $text"
         val process = Runtime.getRuntime().exec(command)
         process.waitFor()
         process.destroy()
 
-        // Upload to firebase
-
         // Send reply message
-        lineMessagingClient.replyMessage(ReplyMessage(replyToken, TextMessage("ok")))
+        lineMessagingClient.replyMessage(ReplyMessage(replyToken, AudioMessage(url + tmpFileName, 60000)))
     }
 
     private fun sendErrorMessage(replyToken: String, message: String) =
             lineMessagingClient.replyMessage(ReplyMessage(replyToken, TextMessage(message)))
 }
+
+@Controller
+class Demo002Controller {
+
+    @GetMapping("/audio/{file}.m4a", produces = ["audio/m4a"])
+    @ResponseBody
+    fun getAudio(@PathVariable file: String): ByteArray {
+        val path = Path.of("/tmp/$file.m4a")
+        if (!Files.exists(path)) {
+            throw NotFoundException()
+        }
+        return Files.readAllBytes(path)
+    }
+}
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class NotFoundException : RuntimeException()
