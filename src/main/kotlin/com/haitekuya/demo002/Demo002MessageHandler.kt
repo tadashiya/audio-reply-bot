7f9eb20189ebb3b16120fa6dda156e31ffbf6719
Tadashi Yamasaki <yamasaki@haitekuya.com>
2018-12-22 16:55:44 +0900

    Update handler code

--- src/main/kotlin/com/haitekuya/demo002/Demo002MessageHandler.kt
+++ src/main/kotlin/com/haitekuya/demo002/Demo002MessageHandler.kt
@@ -9,30 +9,35 @@ import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 
 @LineMessageHandler
-class DemoController(val lineMessagingClient: LineMessagingClient) {
+class Demo002MessageHandler(val lineMessagingClient: LineMessagingClient) {
 
     @EventMapping
     fun handleTextMessageEvent(event: MessageEvent<TextMessageContent>) {
         val text = event.message.text
         val replyToken = event.replyToken
 
-        val replyText = when (text) {
-            "test" -> "test dayo"
-            "test2" -> "test2 dazo"
-            else -> text
+        // check text
+        val regex = Regex("^https://www.youtube.com/watch\\?v=\\w{3,15}$")
+
+        if (!regex.containsMatchIn(text)) {
+            sendErrorMessage(replyToken, "Invalid youtube url")
+            return
         }
 
-        // Check url
         // Download Movie
-        val command = "youtube-dl -x --audio-format m4a -o temp.mp3 https://www.youtube.com/watch?v=c3xxHO1ML5o"
+        val command = "youtube-dl -x --audio-format m4a -o temp.mp3 $text"
         val process = Runtime.getRuntime().exec(command)
         process.waitFor()
         process.destroy()
 
-        // Download Convert to MP3
-        // Send
+        // Upload to firebase
+
+        // Send reply message
 
 
-        lineMessagingClient.replyMessage(ReplyMessage(replyToken, TextMessage(replyText)))
+        lineMessagingClient.replyMessage(ReplyMessage(replyToken, TextMessage("ok")))
     }
+
+    private fun sendErrorMessage(replyToken: String, message: String) =
+            lineMessagingClient.replyMessage(ReplyMessage(replyToken, TextMessage(message)))
 }
